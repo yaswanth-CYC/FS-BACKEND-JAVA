dd40329a1a115eb52ba7d6a4e44f0e0898db8ada
yaswanth-CYC <challayaswanthchowdary123@gmail.com>
2024-08-27 20:11:27 +0530

    java

---
+++ aug 27/A.java
@@ -0,0 +1,7 @@
+class A{
+public static void main(String[] args){
+int a=10;
+a="rahul";
+System.out.println(a);
+} 
+}
---
+++ aug 27/Test.java
@@ -0,0 +1,7 @@
+class Test{
+
+public static void main(String[] args){
+int a=10;
+System.out.println(a);
+} 
+}
