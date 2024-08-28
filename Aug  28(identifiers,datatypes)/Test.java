b03953efdafcf72de45b49f82c9ec52bbe0f382b
yaswanth-CYC <challayaswanthchowdary123@gmail.com>
2024-08-28 21:05:29 +0530

    java

---
+++ Aug  28(identifiers,datatypes)/Test.java
@@ -0,0 +1,22 @@
+class Test{
+       public static void main(String[] args){
+        byte b=10;
+        short s=100;
+        int price=49000;
+        long mobile=2582587852l;
+        float esal1=45000.45f;
+        double esal2=45000.45;
+        double rating=4.5;
+        char avail='y';
+        boolean discount=true;
+        System.out.println(b);
+        System.out.println(s);
+        System.out.println(price);
+        System.out.println(mobile);
+        System.out.println(esal1);
+        System.out.println(esal2);
+        System.out.println(rating);
+        System.out.println(avail);
+        System.out.println(discount);
+    }
+}
